4d80d13e2c1aa3b9f33e1ceb1a14a0db52978b8e
Muhammad Talha <bitf18a543@pucit.edu.pk>
2022-01-21 21:35:41 +0500

    Java Configuration

--- src/main/java/org/talha/mcass03/MainActivity.java
+++ src/main/java/org/talha/mcass03/MainActivity.java
@@ -1,14 +1,36 @@
 package org.talha.mcass03;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout homeRL;
+    private ProgressBar loadingPB;
+    private TextView cityNameTV,temperatureTV,conditionTV;
+    private RecyclerView weatherRV;
+    private TextInputEditText cityEdt;
+    private ImageView backIV,iconIV,searchIV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        homeRL = findViewById(R.id.idRLHome);
+        loadingPB=findViewById(R.id.idPBLoading);
+        cityNameTV=findViewById(R.id.idTVCityName)
+
+
+
+
     }
 }
