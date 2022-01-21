699aac40ded52d3eb864e954314c6079ab6984ba
Muhammad Talha <bitf18a543@pucit.edu.pk>
2022-01-21 21:46:47 +0500

    IDs assign

--- src/main/java/org/talha/mcass03/MainActivity.java
+++ src/main/java/org/talha/mcass03/MainActivity.java
@@ -27,7 +27,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         homeRL = findViewById(R.id.idRLHome);
         loadingPB=findViewById(R.id.idPBLoading);
-        cityNameTV=findViewById(R.id.idTVCityName)
+        cityNameTV=findViewById(R.id.idTVCityName);
+        temperatureTV=findViewById(R.id.idTVTemperature);
+        conditionTV=findViewById(R.id.idTVCondition);
+        weatherRV=findViewById(R.id.idRvWeather);
+        cityEdt=findViewById(R.id.idEdtCity);
+        backIV=findViewById(R.id.idIVBlack);
+        iconIV=findViewById(R.id.idIVIcon);
+        searchIV=findViewById(R.id.idIVSearch);
+
+
 
 
 
